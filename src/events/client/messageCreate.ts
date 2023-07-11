eb57b44d39aa82844dfbef639cb7bcf7ed2e4685
Lanstar <32837891+xLanStar@users.noreply.github.com>
2023-07-12 05:11:29 +0800

    Remove logging

--- src/events/client/messageCreate.ts
+++ src/events/client/messageCreate.ts
@@ -1,13 +1,11 @@
 import { Message } from "discord.js";
 import bot from "../../bot.ts";
-import logger from "../../logger.ts";
 
 export default {
     name: 'messageCreate',
     execute: async (message: Message) => {
         const content = message.content;
         if (message.author.id === bot.user.id) return;
-        logger.info(message);
         // if (content.startsWith('!') && content.indexOf('\n') !== -1) {
         //     const end = content.indexOf('\n');
         //     const lang = content.substring(1, end);
